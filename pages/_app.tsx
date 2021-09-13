06e05b44a373a9e7ff72cd0d1c0b75a79c7878c4
binhphanhai <contact.phbinh@gmail.com>
2021-09-13 11:42:39 +0700

    setup temp layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Newbie Interpreter</title>
+        <meta name="description" content="newbie interpreter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
