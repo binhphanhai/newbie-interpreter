eb3d83e575590127e5526d016092d0874b37121a
binhphanhai <contact.phbinh@gmail.com>
2021-09-24 05:54:09 +0700

    add ActivationRecord

---
+++ helper/callStack/activationRecord.ts
@@ -0,0 +1,43 @@
+export enum ARType {
+  PROGRAM = 0,
+  PROCEDURE = 1,
+}
+
+export class ActivationRecord {
+  name: string;
+  type: ARType;
+  level: number;
+  parentScope: ActivationRecord | null;
+  members: Map<string, any>;
+  builtInProcs: Map<string, string>;
+
+  constructor(
+    name: string,
+    type: ARType,
+    level: number,
+    parentScope: ActivationRecord | null = null
+  ) {
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.parentScope = parentScope;
+    this.members = new Map<string, any>();
+    this.builtInProcs = new Map<string, string>();
+    this.initBuiltInProcs();
+  }
+
+  private initBuiltInProcs() {
+    this.members.set("print", "VisitPrint");
+    this.builtInProcs.set("print", "VisitPrint");
+  }
+
+  public getItem(key: string): any {
+    if (this.members.has(key)) return this.members.get(key);
+    else if (this.parentScope !== null) return this.parentScope.getItem(key);
+    else return null;
+  }
+
+  public setItem(key: string, value: any) {
+    this.members.set(key, value);
+  }
+}
