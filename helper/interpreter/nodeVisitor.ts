20df80277eb157b5283c43d79510a1e16269f571
binhphanhai <contact.phbinh@gmail.com>
2021-09-28 20:33:29 +0700

    update[NodeVisitor]: add "this" before method to call self method

--- helper/interpreter/nodeVisitor.ts
+++ helper/interpreter/nodeVisitor.ts
@@ -1,9 +1,9 @@
 import AST from "../abstractSyntaxTree/ast";
 
 export default class NodeVisitor {
-  public Visit(node: AST): any {
+  public visit(node: AST): any {
     const nodeType = node.constructor.name;
-    const method = `Visit${nodeType}(node)`;
+    const method = `this.visit${nodeType}(node)`;
     eval(method);
   }
 }
