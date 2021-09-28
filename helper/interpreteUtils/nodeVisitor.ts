d85efca3d9e13eb4d42985b0602d78d783fd325d
binhphanhai <contact.phbinh@gmail.com>
2021-09-29 06:17:15 +0700

    update[NodeVisitor]: return value

--- helper/interpreteUtils/nodeVisitor.ts
+++ helper/interpreteUtils/nodeVisitor.ts
@@ -4,6 +4,6 @@ export default class NodeVisitor {
   public visit(node: AST): any {
     const nodeType = node.getType();
     const method = `this.visit${nodeType}(node)`;
-    eval(method);
+    return eval(method);
   }
 }
